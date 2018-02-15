1ff3433e860b95bd801591b7cd1fa89912052f17
Joe Criscuolo <joseph.criscuolo@wework.com>
2018-02-15 02:27:52 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index
-  user_input = gets.chomp.to_i
+  user_input = gets.to_i
   converted_input = input_to_index(user_input)
   
 end
